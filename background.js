f39d9b7482a538394fdaae6fbc6d619b6de41ae4
GamingPrower <dcody12@hotmail.com>
2023-05-13 00:41:24 -0400

    Init Commit

--- background.js
+++ background.js
@@ -1,12 +1,115 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
+import './jszip.min.js';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "add_to_list",
+    title: "Add Image to List",
+    contexts: ["image", "link"],
+    targetUrlPatterns:
+      ["*://*/*.jpg",
+      "*://*/*.jpeg",
+      "*://*/*.png",
+      "*://*/*.gif",
+      "*://*/*.bmp",
+      "*://*/*.svg"]
   });
-  
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "add_to_list") {
+    chrome.storage.session.get({ images: [] }, result => {
+      const images = result.images;
+      images.push(info.srcUrl ? info.srcUrl : info.linkUrl);
+      chrome.storage.session.set({ images: images });
+
+      console.log(images);
+    });
+  }
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.command === "download_all") {
+    chrome.storage.session.get({ images: [] }, result => {
+      const images = [...new Set(result.images)];
+      if (images.length > 0) {
+
+        images.forEach((imageUrl, index) => {
+          chrome.downloads.download({ url: imageUrl, filename: `image_${index}.${imageUrl.split(".").pop()}`, saveAs: false });
+        });
+
+        sendResponse({ success: true });
+      }
+    });
+  }
+
+  if (message.command === "download_as_zip") {
+    chrome.storage.session.get({ images: [] }, result => {
+      const images = [...new Set(result.images)];
+      if (images.length > 0) {
+
+        const zip = new JSZip();
+        const folder = zip.folder("images")
+        const downloadPromises = [];
+
+        images.forEach((imageUrl, index) => {
+          const filename = `image_${index}.png`;
+          const downloadPromise = fetch(imageUrl)
+            .then(async response => {
+              const blob = await response.blob();
+              folder.file(filename, blob, { binary: true });
+              return response.blob;
+            });
+
+            downloadPromises.push(downloadPromise);
+        });
+
+        Promise.all(downloadPromises).then(() => {
+          zip.generateAsync({ type: "base64" })
+            .then(content => {
+
+              chrome.downloads.download({
+                url: `data:application/octet-stream;base64,${content}`,
+                filename: "images.zip"
+              });
+
+              sendResponse({ success: true });
+            });
+        });
+      }
+    });
+  }
+
+  if (message.command === "add_all_tabs") {
+    chrome.tabs.query({}, (tabs) => {
+      chrome.storage.session.get({ images: [] }, result => {
+        const images = result.images;
+
+        tabs.forEach(({ url }) => {
+          if (url && url.match(/\bhttps?:\/\/\S+?\.(?:jpg|jpeg|png|gif|bmp|svg)\b/)) {
+            images.push(url);
+          }
+        });
+
+        chrome.storage.session.set({ images: images });
+
+        sendResponse({ success: true });
+      });
+    });
+  }
+
+  if (message.command === "clear_images") {
+    chrome.storage.session.clear(() =>{
+      console.log("Images Cleared!");
+    });
+  }
+
+  if (message.command === "get_images") {
+    chrome.storage.session.get({ images: [] }, result => {
+      console.log(result.images);
+      console.log("Sending...")
+      sendResponse({ images: result.images });
+    });
+  }
+
+  return true;
+});
--- popup.js
+++ popup.js
@@ -1,12 +1,79 @@
-// adding a new bookmark row to the popup
-const addNewBookmark = () => {};
+// chrome.runtime.onMessage.addListener(message => {
+// 	if (message.url) {
+// 		chrome.downloads.download({
+// 			url: message.url,
+// 			filename: "images.zip"
+// 		});
+// 	}
+// });
 
-const viewBookmarks = () => {};
+const downloadAllImages = () => {
+	return new Promise(resolve => {
+		chrome.runtime.sendMessage({ command: "download_all" }, response => {
+			resolve({ success: response.success });
+		});
+	});
+};
 
-const onPlay = e => {};
+const downloadAsZip = () => {
+	return new Promise(resolve => {
+		chrome.runtime.sendMessage({ command: "download_as_zip" }, response => {
+			resolve({ success: response.success });
+		});
+	});
+};
 
-const onDelete = e => {};
+const addAllTabs = () => {
+	return new Promise(resolve => {
+		chrome.runtime.sendMessage({ command: "add_all_tabs" }, response => {
+			resolve({ success: response.success });
+		});
+	});
+};
 
-const setBookmarkAttributes =  () => {};
+const clearImages = () => {
+	chrome.runtime.sendMessage({ command: "clear_images" }, response => {
+		// stuff
+	});
+};
 
-document.addEventListener("DOMContentLoaded", () => {});
+const getImageCount = () => {
+	return new Promise(resolve => {
+		chrome.runtime.sendMessage({ command: "get_images" }, response => {
+			resolve(response ? [...new Set(response.images)].length : 0);
+		});
+	});
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+	const imageCount = await getImageCount();
+
+	console.log(imageCount);
+
+	const imageCountText = document.getElementById("imageCount");
+	const downloadImagesButton = document.getElementById("downloadImages");
+	const downloadButton = document.getElementById("downloadButton");
+	const allTabsButton = document.getElementById("allTabs");
+	const clearButton = document.getElementById("clear");
+
+	imageCountText.innerText = imageCount
+
+	downloadImagesButton.addEventListener("click", async () => {
+		await downloadAllImages();
+		clearImages();
+		imageCountText.innerText = await getImageCount();
+	});
+	downloadButton.addEventListener("click", async () => {
+		await downloadAsZip();
+		clearImages();
+		imageCountText.innerText = await getImageCount();
+	});
+	allTabsButton.addEventListener("click", async () => {
+		await addAllTabs();
+		imageCountText.innerText = await getImageCount();
+	});
+	clearButton.addEventListener("click", async () => {
+		clearImages();
+		imageCountText.innerText = await getImageCount();
+	});
+});
